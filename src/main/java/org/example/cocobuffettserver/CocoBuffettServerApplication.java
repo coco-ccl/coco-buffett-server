4b4be6521d644d1287ca18b09e09045ac1cf4adb
zoyul <p.zoyul@gmail.com>
2025-12-14 21:05:19 +0900

    feat: project setting

---
+++ src/main/java/org/example/cocobuffettserver/CocoBuffettServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.cocobuffettserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoBuffettServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocoBuffettServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cocobuffettserver/CocoBuffettServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cocobuffettserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocoBuffettServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
